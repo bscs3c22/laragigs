2d93c1f8e35e81e67d7d657847d6d714500edf31
marky <marky@test.com>
2022-10-22 13:12:31 +0800

    Modified migration

--- database/migrations/2022_10_09_012739_create_products_table.php
+++ database/migrations/2022_10_09_012739_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('product_name')->unique();
-            $table->string('image')->nullable();
+            $table->string('logo')->nullable();
             $table->longText('description');
             $table->string('tags');
             $table->double('price', 8, 2);
