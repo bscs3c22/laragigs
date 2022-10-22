74106e76a6b16bd0bee3a6694316d070663a4e94
marky <marky@test.com>
2022-10-22 15:51:15 +0800

    filesystems file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -54,7 +54,7 @@ class ProductController extends Controller
         ]);
 
         if($request->hasFile('logo')){
-            $formFields['logo'] = $request->file('logo')->store('logos');
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         }
 
         Product::create($formFields);
